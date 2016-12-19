3ea81839c232c1728d51777ac62df6d736b4b1ed
jsEklund <eklund84@gmail.com>
2016-12-20 00:45:38 +0100

    Prepare day 3

---
+++ src/day3/day3_1.ts
@@ -0,0 +1,22 @@
+namespace day3_1 {
+
+    var fs = require("fs");
+    var data = fs.readFileSync('src/day2/input.txt', 'utf8');
+
+    //
+
+    function getInputArray(input: string) {
+        return input.split("\n")
+    }
+
+    export class Startup {
+
+        public static main() {
+
+            // run
+
+        }
+    }
+}
+
+day3_1.Startup.main();
