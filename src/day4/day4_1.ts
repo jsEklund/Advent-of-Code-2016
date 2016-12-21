a86102436ab94838dd99620ae23f19fad2d262f7
jsEklund <eklund84@gmail.com>
2016-12-22 00:20:44 +0100

    Prepare day 4

---
+++ src/day4/day4_1.ts
@@ -0,0 +1,20 @@
+namespace day4_1 {
+
+    var fs = require("fs");
+    var data = fs.readFileSync('src/day4/input.txt', 'utf8');
+
+
+    function getInputArray(input: string) {
+        return input.split("\n")
+    }
+
+    export class Startup {
+
+        public static main() {
+
+
+        }
+    }
+}
+
+day4_1.Startup.main();
---
+++ src/day4/day4_2.ts
@@ -0,0 +1,20 @@
+namespace day4_2 {
+
+    var fs = require("fs");
+    var data = fs.readFileSync('src/day4/input.txt', 'utf8');
+
+
+    function getInputArray(input: string) {
+        return input.split("\n")
+    }
+
+    export class Startup {
+
+        public static main() {
+
+
+        }
+    }
+}
+
+day4_2.Startup.main();
