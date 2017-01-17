ec97d5388fb0b7c0a63357602d1c9e8ab2c8ae4e
jsEklund <eklund84@gmail.com>
2017-01-17 23:53:17 +0100

    Prepare day 6

---
+++ src/day6/day6_1.ts
@@ -0,0 +1,16 @@
+import crypto = require('crypto');
+
+namespace day6_1 {
+    
+    var fs = require("fs");
+    var data = fs.readFileSync('src/day6/input.txt', 'utf8');
+
+    export class Startup {
+
+        public static main() {
+
+        }
+    }
+}
+
+day6_1.Startup.main();
